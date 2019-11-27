638ab980bf2d3b29469cc72059f378ea0ab1eab9
paingheinhtet <paingheinhtet95@gmail.com>
2019-11-27 20:09:53 +0630

    auth made

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\DB;
 
 class RecordController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware ('role:admin');
+    }
     /**
      * Display a listing of the resource.
      *
